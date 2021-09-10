5998f821750c3eed6c8322af9c83bd6a2355fb86
Yes005 <yeshuab3@gmail.com>
2021-09-10 12:08:53 -0600

    Change AirportTest

--- src/main/java/OutputandInputs/AirportInputsTest.java
+++ src/main/java/OutputandInputs/AirportInputsTest.java
@@ -1,5 +1,10 @@
 package OutputandInputs;
 
+import AirportFlight.Airport;
+
+import java.util.List;
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -9,7 +14,13 @@ class AirportInputsTest {
 
     @org.junit.jupiter.api.Test
     void testGetDatAirport() {
+        Airport listAirport = airportInputs.getDatAirport();
+        listAirport.setId("1515");
+        listAirport.setName("Aeropuerto El Salvador");
+        listAirport.setCity("La Paz");
+        listAirport.setCountry("El Salvador");
 
-        assertTrue(airportInputs.getDatAirport() != null,"El arreglo no viene vacio");
+
+        assertTrue(airportInputs.getDatAirport() == listAirport);
     }
 }
